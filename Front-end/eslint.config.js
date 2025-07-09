d2fa96c79a590fddaf37fd0ae423963689634912
Wallace Martins <108425466+IWMVI@users.noreply.github.com>
2025-07-09 00:28:43 -0300

    feat: adicionar configuração do ESLint para padronização de código JavaScript

---
+++ Front-end/eslint.config.js
@@ -0,0 +1,16 @@
+module.exports = [
+    {
+        files: ["**/*.js"],
+        languageOptions: {
+            ecmaVersion: 2021,
+            sourceType: "module",
+        },
+        rules: {
+            "no-unused-vars": "warn",
+            eqeqeq: "error",
+            "no-console": "off",
+            semi: ["error", "always"],
+            quotes: ["error", "single"],
+        },
+    },
+];
