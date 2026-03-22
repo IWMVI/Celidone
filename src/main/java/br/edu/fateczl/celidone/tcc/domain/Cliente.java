c0840e2d2feefdf831dbe37ddf55abb981d7f113
Wallace Martins <wmvwallace@gmail.com>
2026-03-22 00:13:03 -0300

    refactor: remove Lombok das entidades de domínio

--- src/main/java/br/edu/fateczl/celidone/tcc/domain/Cliente.java
+++ src/main/java/br/edu/fateczl/celidone/tcc/domain/Cliente.java
@@ -7,21 +7,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
-@Data
-@Builder
 @Table(name = "cliente")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 public class Cliente {
 
     @Id
@@ -47,6 +39,72 @@ public class Cliente {
     @CreationTimestamp
     private LocalDate dataCadastro;
 
+    public Cliente() {
+    }
+
+    public Cliente(String nome, String cpfCnpj, String email, String celular, Endereco endereco) {
+        this.nome = nome;
+        this.cpfCnpj = cpfCnpj;
+        this.email = email;
+        this.celular = celular;
+        this.endereco = endereco;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpfCnpj() {
+        return cpfCnpj;
+    }
+
+    public void setCpfCnpj(String cpfCnpj) {
+        this.cpfCnpj = cpfCnpj;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDate dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
 
     public void atualizar(
             String nome,
@@ -60,5 +118,93 @@ public class Cliente {
         this.email = email;
         this.celular = celular;
         this.endereco = endereco;
+    }
+
+    public static ClienteBuilder builder() {
+        return new ClienteBuilder();
+    }
+
+    public static class ClienteBuilder {
+        private Long id;
+        private String nome;
+        private String cpfCnpj;
+        private String email;
+        private String celular;
+        private Endereco endereco;
+        private LocalDate dataCadastro;
+
+        public ClienteBuilder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public ClienteBuilder nome(String nome) {
+            this.nome = nome;
+            return this;
+        }
+
+        public ClienteBuilder cpfCnpj(String cpfCnpj) {
+            this.cpfCnpj = cpfCnpj;
+            return this;
+        }
+
+        public ClienteBuilder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public ClienteBuilder celular(String celular) {
+            this.celular = celular;
+            return this;
+        }
+
+        public ClienteBuilder endereco(Endereco endereco) {
+            this.endereco = endereco;
+            return this;
+        }
+
+        public ClienteBuilder dataCadastro(LocalDate dataCadastro) {
+            this.dataCadastro = dataCadastro;
+            return this;
+        }
+
+        public Cliente build() {
+            Cliente cliente = new Cliente();
+            cliente.id = this.id;
+            cliente.nome = this.nome;
+            cliente.cpfCnpj = this.cpfCnpj;
+            cliente.email = this.email;
+            cliente.celular = this.celular;
+            cliente.endereco = this.endereco;
+            cliente.dataCadastro = this.dataCadastro;
+            return cliente;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(id, cliente.id) &&
+                Objects.equals(cpfCnpj, cliente.cpfCnpj);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, cpfCnpj);
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", cpfCnpj='" + cpfCnpj + '\'' +
+                ", email='" + email + '\'' +
+                ", celular='" + celular + '\'' +
+                ", endereco=" + endereco +
+                ", dataCadastro=" + dataCadastro +
+                '}';
     }
 }
--- src/main/java/br/edu/fateczl/celidone/tcc/domain/ItemAluguel.java
+++ src/main/java/br/edu/fateczl/celidone/tcc/domain/ItemAluguel.java
@@ -9,17 +9,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 @Entity(name = "item_aluguel")
 @Table(name = "item_aluguel")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class ItemAluguel {
 
     @Id
@@ -32,7 +27,6 @@ public class ItemAluguel {
     @Column(precision = 8, scale = 2, nullable = false)
     private BigDecimal subtotal;
 
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_aluguel", nullable = false)
     private Aluguel aluguel;
@@ -41,11 +35,85 @@ public class ItemAluguel {
     @JoinColumn(name = "id_traje", nullable = false)
     private Traje traje;
 
+    public ItemAluguel() {
+    }
+
+    public ItemAluguel(Long id, int quantidade, BigDecimal subtotal, Aluguel aluguel, Traje traje) {
+        this.id = id;
+        this.quantidade = quantidade;
+        this.subtotal = subtotal;
+        this.aluguel = aluguel;
+        this.traje = traje;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(BigDecimal subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public Aluguel getAluguel() {
+        return aluguel;
+    }
+
+    public void setAluguel(Aluguel aluguel) {
+        this.aluguel = aluguel;
+    }
+
+    public Traje getTraje() {
+        return traje;
+    }
+
+    public void setTraje(Traje traje) {
+        this.traje = traje;
+    }
 
     public void atualizar(int quantidade, BigDecimal subtotal, Aluguel aluguel, Traje traje) {
         this.quantidade = quantidade;
         this.subtotal = subtotal;
         this.aluguel = aluguel;
         this.traje = traje;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ItemAluguel that = (ItemAluguel) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "ItemAluguel{" +
+                "id=" + id +
+                ", quantidade=" + quantidade +
+                ", subtotal=" + subtotal +
+                ", aluguel=" + aluguel +
+                ", traje=" + traje +
+                '}';
     }
 }
