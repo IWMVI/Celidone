6a74417f4a05c0585f4ffbf528102a7692ed0bce
Wallace Martins <108425466+IWMVI@users.noreply.github.com>
2026-03-21 23:03:52 -0300

    feat: melhora integração e serialização de enums dos estados

--- src/main/java/br/edu/fateczl/celidone/tcc/enums/SiglaEstados.java
+++ src/main/java/br/edu/fateczl/celidone/tcc/enums/SiglaEstados.java
@@ -1,31 +1,44 @@
 package br.edu.fateczl.celidone.tcc.enums;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum SiglaEstados {
-    AC,
-    AL,
-    AP,
-    AM,
-    BA,
-    CE,
-    DF,
-    ES,
-    GO,
-    MA,
-    MT,
-    MS,
-    MG,
-    PA,
-    PB,
-    PR,
-    PE,
-    PI,
-    RJ,
-    RN,
-    RS,
-    RO,
-    RR,
-    SC,
-    SP,
-    SE,
-    TO
+    AC("AC"),
+    AL("AL"),
+    AP("AP"),
+    AM("AM"),
+    BA("BA"),
+    CE("CE"),
+    DF("DF"),
+    ES("ES"),
+    GO("GO"),
+    MA("MA"),
+    MT("MT"),
+    MS("MS"),
+    MG("MG"),
+    PA("PA"),
+    PB("PB"),
+    PR("PR"),
+    PE("PE"),
+    PI("PI"),
+    RJ("RJ"),
+    RN("RN"),
+    RS("RS"),
+    RO("RO"),
+    RR("RR"),
+    SC("SC"),
+    SP("SP"),
+    SE("SE"),
+    TO("TO");
+
+    private final String valor;
+
+    SiglaEstados(String valor) {
+        this.valor = valor;
+    }
+
+    @JsonValue
+    public String getValor() {
+        return valor;
+    }
 }
