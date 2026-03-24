915d1f8fb42838c38c9168c13dee982b6950cf6f
Pedro <pedromendes7182@gmail.com>
2026-03-24 19:41:23 -0300

    feat(medida): aumenta o tamanho das medidas de 99.9 para 999.99 nas entidades e requests

--- src/main/java/br/edu/fateczl/tcc/dto/feminina/MedidaFemininaRequest.java
+++ src/main/java/br/edu/fateczl/tcc/dto/feminina/MedidaFemininaRequest.java
@@ -11,47 +11,48 @@ public record MedidaFemininaRequest(
         @NotNull(message = "ID do cliente é obrigatório")
         Long clienteId,
 
-        @NotNull(message = "Cintura é obrigatória")
-        @Digits(integer = 3, fraction = 1, message = "A medida da cintura deve ter no máximo 4 dígitos e 1 decimal")
+        @NotNull(message = "A medida da cintura é obrigatória")
+        @Digits(integer = 5, fraction = 2, message = "A medida da cintura deve ter no máximo 5 dígitos no total, sendo até 2 casas decimais (ex: 999.99)")
         @Positive(message = "O valor da medida da cintura deve ser positivo")
         BigDecimal cintura,
 
-        @NotNull(message = "Manga é obrigatória")
-        @Digits(integer = 3, fraction = 1, message = "A medida da manga deve ter no máximo 4 dígitos e 1 decimal")
+        @NotNull(message = "A medida da manga é obrigatória")
+        @Digits(integer = 5, fraction = 2, message = "A medida da manga deve ter no máximo 5 dígitos no total, sendo até 2 casas decimais (ex: 999.99)")
         @Positive(message = "O valor da medida da manga deve ser positivo")
         BigDecimal manga,
 
-        @NotNull(message = "Altura do busto é obrigatória")
-        @Digits(integer = 3, fraction = 1, message = "A medida do busto deve ter no máximo 4 dígitos e 1 decimal")
+        @NotNull(message = "A altura do busto é obrigatória")
+        @Digits(integer = 5, fraction = 2, message = "A medida do busto deve ter no máximo 5 dígitos no total, sendo até 2 casas decimais (ex: 999.99)")
+        @Positive(message = "O valor da medida do busto deve ser positivo")
         BigDecimal alturaBusto,
 
-        @NotNull(message = "Raio do busto é obrigatório")
-        @Digits(integer = 3, fraction = 1, message = "O raio do busto deve ter no máximo 4 dígitos e 1 decimal")
+        @NotNull(message = "O raio do busto é obrigatório")
+        @Digits(integer = 5, fraction = 2, message = "A medida do raio do busto deve ter no máximo 5 dígitos no total, sendo até 2 casas decimais (ex: 999.99)")
         @Positive(message = "O valor da medida do raio do busto deve ser positivo")
         BigDecimal raioBusto,
 
-        @NotNull(message = "Corpo é obrigatório")
-        @Digits(integer = 3, fraction = 1, message = "A altura do corpo deve ter no máximo 4 dígitos e 1 decimal")
+        @NotNull(message = "A altura do corpo é obrigatória")
+        @Digits(integer = 5, fraction = 2, message = "A altura do corpo deve ter no máximo 5 dígitos no total, sendo até 2 casas decimais (ex: 999.99)")
         @Positive(message = "O valor da altura do corpo deve ser positivo")
         BigDecimal corpo,
 
-        @NotNull(message = "Ombro é obrigatório")
-        @Digits(integer = 3, fraction = 1, message = "A largura do ombro deve ter no máximo 4 dígitos e 1 decimal")
+        @NotNull(message = "A medida do ombro é obrigatória")
+        @Digits(integer = 5, fraction = 2, message = "A largura do ombro deve ter no máximo 5 dígitos no total, sendo até 2 casas decimais (ex: 999.99)")
         @Positive(message = "O valor da largura do ombro deve ser positivo")
         BigDecimal ombro,
 
-        @NotNull(message = "Decote é obrigatório")
-        @Digits(integer = 3, fraction = 1, message = "A medida do decote deve ter no máximo 4 dígitos e 1 decimal")
+        @NotNull(message = "A medida do decote é obrigatória")
+        @Digits(integer = 5, fraction = 2, message = "A medida do decote deve ter no máximo 5 dígitos no total, sendo até 2 casas decimais (ex: 999.99)")
         @Positive(message = "O valor da medida do decote deve ser positivo")
         BigDecimal decote,
 
-        @NotNull(message = "Quadril é obrigatório")
-        @Digits(integer = 3, fraction = 1, message = "A medida do quadril deve ter no máximo 4 dígitos e 1 decimal")
+        @NotNull(message = "A medida do quadril é obrigatória")
+        @Digits(integer = 5, fraction = 2, message = "A medida do quadril deve ter no máximo 5 dígitos no total, sendo até 2 casas decimais (ex: 999.99)")
         @Positive(message = "O valor da medida do quadril deve ser positivo")
         BigDecimal quadril,
 
-        @NotNull(message = "Comprimento do vestido é obrigatório")
-        @Digits(integer = 3, fraction = 1, message = "O comprimento do vestido deve ter no máximo 4 dígitos e 1 decimal")
+        @NotNull(message = "O comprimento do vestido é obrigatório")
+        @Digits(integer = 5, fraction = 2, message = "O comprimento do vestido deve ter no máximo 5 dígitos no total, sendo até 2 casas decimais (ex: 999.99)")
         @Positive(message = "O valor da medida do comprimento do vestido deve ser positivo")
         BigDecimal comprimentoVestido
 
