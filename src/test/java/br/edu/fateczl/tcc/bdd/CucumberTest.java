7a28123e62df1ff323454f65c104f9b50469494e
Wallace Martins <wmvwallace@gmail.com>
2026-03-26 00:47:34 -0300

    feat(bdd): adiciona testes BDD Cucumber para gerenciamento de medidas
    Configuracao:
    - Adiciona AllCucumberTests.java como runner principal
    - Atualiza CucumberTest.java para usar configuracao Spring
    - Atualiza CucumberSpringConfiguration.java com classes corretas
    - Adiciona MedidaSteps.java com step definitions
    
    Feature de medidas corporais (17 cenarios):
    - Criar medida masculina com dados validos
    - Falha ao criar medida masculina com cliente inexistente
    - Falha ao criar medida masculina com medida negativa
    - Criar medida feminina com dados validos
    - Falha ao criar medida feminina com cliente inexistente
    - Buscar medida masculina por ID existente
    - Buscar medida feminina por ID existente
    - Falha ao buscar medida inexistente
    - Listar todas as medidas
    - Listar medidas filtradas por cliente
    - Listar medidas filtradas por sexo
    - Atualizar medida masculina com dados validos
    - Atualizar medida feminina com dados validos
    - Falha ao atualizar medida inexistente
    - Deletar medida masculina
    - Deletar medida feminina
    - Falha ao deletar medida inexistente

--- src/test/java/br/edu/fateczl/tcc/bdd/CucumberTest.java
+++ src/test/java/br/edu/fateczl/tcc/bdd/CucumberTest.java
@@ -1,15 +1,23 @@
 package br.edu.fateczl.tcc.bdd;
 
+import io.cucumber.spring.CucumberContextConfiguration;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
+@CucumberContextConfiguration
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 @Suite
 @SelectClasspathResource("features/clientes")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "br.edu.fateczl.tcc.bdd.steps")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty,html:build/reports/cucumber/cucumber.html")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty,html:build/reports/cucumber/clientes.html")
 public class CucumberTest {
 }
