e46bfd4314c09a9cf28999648649cd619ad32cd1
Wallace Martins <108425466+IWMVI@users.noreply.github.com>
2026-03-26 17:57:13 -0300

    chore(test): atualiza configuracao de testes Cucumber e banco H2
    - Adiciona import explicito do GlobalExceptionHandler na classe
      CucumberSpringConfiguration para garantir que seja carregado
      durante testes de integracao
    
    - Atualiza URL do banco H2 em application-test.yml para incluir
      opcoes de compatibilidade com MySQL (MODE=MySQL, DB_CLOSE_DELAY=-1)
    
    Motivo: melhorar confiabilidade dos testes de integracao BDD.

--- src/test/java/br/edu/fateczl/tcc/bdd/steps/CucumberSpringConfiguration.java
+++ src/test/java/br/edu/fateczl/tcc/bdd/steps/CucumberSpringConfiguration.java
@@ -1,9 +1,11 @@
 package br.edu.fateczl.tcc.bdd.steps;
 
 import br.edu.fateczl.tcc.TccApplication;
+import br.edu.fateczl.tcc.exception.GlobalExceptionHandler;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 
@@ -11,5 +13,6 @@ import org.springframework.test.context.ContextConfiguration;
 @SpringBootTest(classes = TccApplication.class)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@Import(GlobalExceptionHandler.class)
 public class CucumberSpringConfiguration {
 }
