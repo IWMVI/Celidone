049874100b85428604369cd5f1a15dccae174fce
Wallace Martins <wmvwallace@gmail.com>
2026-03-26 00:47:34 -0300

    feat(bdd): adiciona testes BDD Cucumber para gerenciamento de medidas
    Configuracao:
    - Adiciona AllCucumberTests.java como runner principal
    - Atualiza CucumberTest.java para usar configuracao Spring
    - Atualiza CucumberSpringConfiguration.java com classes corretas
    - Adiciona MedidaSteps.java com step definitions
    
    Feature de medidas corporais (17 cenarios):
    - Criar medida masculina com dados validos
    - Falha ao criar medida masculina com cliente inexistente
    - Falha ao criar medida masculina com medida negativa
    - Criar medida feminina com dados validos
    - Falha ao criar medida feminina com cliente inexistente
    - Buscar medida masculina por ID existente
    - Buscar medida feminina por ID existente
    - Falha ao buscar medida inexistente
    - Listar todas as medidas
    - Listar medidas filtradas por cliente
    - Listar medidas filtradas por sexo
    - Atualizar medida masculina com dados validos
    - Atualizar medida feminina com dados validos
    - Falha ao atualizar medida inexistente
    - Deletar medida masculina
    - Deletar medida feminina
    - Falha ao deletar medida inexistente

--- src/test/java/br/edu/fateczl/tcc/bdd/steps/CucumberSpringConfiguration.java
+++ src/test/java/br/edu/fateczl/tcc/bdd/steps/CucumberSpringConfiguration.java
@@ -1,12 +1,14 @@
 package br.edu.fateczl.tcc.bdd.steps;
 
+import br.edu.fateczl.tcc.TccApplication;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
 @CucumberContextConfiguration
-@SpringBootTest
+@SpringBootTest(classes = TccApplication.class)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 public class CucumberSpringConfiguration {
